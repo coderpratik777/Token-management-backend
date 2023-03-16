fe952b54befe0dac934f6bf53b674958426dd19e
Ajinkya Ashokrao Pawar <127041189+AjinkyaAPawar@users.noreply.github.com>
2023-03-16 23:27:43 +0530

    Manager Services

--- Bank-Token-Management-System/src/main/java/com/pratiti/project/repository/ServicetypeRepository.java
+++ Bank-Token-Management-System/src/main/java/com/pratiti/project/repository/ServicetypeRepository.java
@@ -1,16 +1,30 @@
+// package com.pratiti.project.repository;
+
+// import java.util.Optional;
+
+// import org.springframework.data.jpa.repository.JpaRepository;
+// import org.springframework.data.jpa.repository.Query;
+
+// import com.pratiti.project.entity.Servicetype;
+
+// public interface ServicetypeRepository extends JpaRepository<Servicetype, Integer>{
+	
+	
+// 	@Query("select st from Servicetype st where st.serviceName=?1")
+// 	Optional<Servicetype> findByServiceName(String x);
+
+// }
+
 package com.pratiti.project.repository;
 
 import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-
 import com.pratiti.project.entity.Servicetype;
 
 public interface ServicetypeRepository extends JpaRepository<Servicetype, Integer>{
-	
-	
-	@Query("select st from Servicetype st where st.serviceName=?1")
-	Optional<Servicetype> findByServiceName(String x);
+
+	@Query("select s from Servicetype s where upper(s.serviceName) like upper(?1)")
+	Optional<Servicetype> findByServiceName(String serviceName);
 
 }
