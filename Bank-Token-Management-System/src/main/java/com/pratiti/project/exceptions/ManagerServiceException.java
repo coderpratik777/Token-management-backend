23602339ff8bc8b892030ae66c9babadf427357a
Ajinkya Ashokrao Pawar <127041189+AjinkyaAPawar@users.noreply.github.com>
2023-03-16 20:40:26 +0530

    Add Counter

--- Bank-Token-Management-System/src/main/java/com/pratiti/project/exceptions/ManagerServiceException.java
+++ Bank-Token-Management-System/src/main/java/com/pratiti/project/exceptions/ManagerServiceException.java
@@ -2,4 +2,9 @@ package com.pratiti.project.exceptions;
 
 public class ManagerServiceException extends RuntimeException{
 
+	public ManagerServiceException(String string) {
+		// TODO Auto-generated constructor stub
+		super(string);
+	}
+
 }
