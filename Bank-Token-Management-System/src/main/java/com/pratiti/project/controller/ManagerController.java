b8e98cab1ef49ecfebd79646e6ee647d5a0d982d
Ajinkya Ashokrao Pawar <127041189+AjinkyaAPawar@users.noreply.github.com>
2023-03-16 20:37:34 +0530

    Add Counter

--- Bank-Token-Management-System/src/main/java/com/pratiti/project/controller/ManagerController.java
+++ Bank-Token-Management-System/src/main/java/com/pratiti/project/controller/ManagerController.java
@@ -1,12 +1,31 @@
 package com.pratiti.project.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import com.pratiti.project.entity.Counter;
+import com.pratiti.project.exceptions.ManagerServiceException;
 import com.pratiti.project.service.ManagerService;
 
+@RestController
+@CrossOrigin
 public class ManagerController {
 	
 	@Autowired
 	private ManagerService managerService;
+	
+	@PostMapping("/add/counter")
+	public String addCounter(@RequestBody Counter counter) {
+		try {
+		     managerService.addCounter(counter);
+		     return "Counter Added Succesfully";
+		} catch (ManagerServiceException e) {
+			return (e.getMessage());
+		}
+	}
+	
+	
 	
 }
