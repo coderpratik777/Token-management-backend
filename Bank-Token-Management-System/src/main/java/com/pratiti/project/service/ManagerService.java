ce1abcfc5162e36382412af61896a51e8cda1c56
Ajinkya Ashokrao Pawar <127041189+AjinkyaAPawar@users.noreply.github.com>
2023-03-16 20:38:37 +0530

    Add Counter

--- Bank-Token-Management-System/src/main/java/com/pratiti/project/service/ManagerService.java
+++ Bank-Token-Management-System/src/main/java/com/pratiti/project/service/ManagerService.java
@@ -1,10 +1,25 @@
 package com.pratiti.project.service;
 
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import com.pratiti.project.entity.Counter;
+import com.pratiti.project.exceptions.ManagerServiceException;
+import com.pratiti.project.repository.CounterRepository;
 
 @Service
 public class ManagerService {
 
-	
+	@Autowired
+	private CounterRepository counterRepository;
+
+	public void addCounter(Counter counter) {
+		Optional<Counter> counter1 = counterRepository.findByName(counter.getName());
+		if (!counter1.isPresent()) {
+			counterRepository.save(counter);
+		} else {
+			throw new ManagerServiceException("Counter Is already Available");
+		}
+	}
 
 }
