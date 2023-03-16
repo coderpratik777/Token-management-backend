4dcfb007cb0cca474d849c258e772d06aacd9fe7
Ajinkya Ashokrao Pawar <127041189+AjinkyaAPawar@users.noreply.github.com>
2023-03-16 23:18:40 +0530

    Manager Update

--- Bank-Token-Management-System/src/main/java/com/pratiti/project/controller/ManagerController.java
+++ Bank-Token-Management-System/src/main/java/com/pratiti/project/controller/ManagerController.java
@@ -5,27 +5,54 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
 import com.pratiti.project.entity.Counter;
+import com.pratiti.project.entity.Service;
+import com.pratiti.project.entity.Servicetype;
 import com.pratiti.project.exceptions.ManagerServiceException;
 import com.pratiti.project.service.ManagerService;
 
 @RestController
 @CrossOrigin
 public class ManagerController {
-	
+
 	@Autowired
 	private ManagerService managerService;
+
 	
+	// Manager : Adding Counter
 	@PostMapping("/add/counter")
 	public String addCounter(@RequestBody Counter counter) {
 		try {
-		     managerService.addCounter(counter);
-		     return "Counter Added Succesfully";
+			managerService.addCounter(counter);
+			return "Counter Added Succesfully";
 		} catch (ManagerServiceException e) {
 			return (e.getMessage());
 		}
 	}
+
 	
+	// Manager : Adding Service
+	@PostMapping("/add/service")
+	public int addService(@RequestBody Service service) {
+		try {
+			int id = managerService.addService(service);
+			return id;
+		} catch (ManagerServiceException e) {
+			return 0;
+		}
+	}
+
 	
-	
+	// Manager : Adding Type Of Service
+	@PostMapping("/add/service/type")
+	public String addServiceType(@RequestBody Servicetype serviceType) {
+		try {
+			managerService.addServiceType(serviceType);
+			return "Service Type Added Successfully!";
+
+		} catch (ManagerServiceException e) {
+			return (e.getMessage());
+		}
+	}
 }
